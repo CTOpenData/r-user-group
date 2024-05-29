9c1be8019b31caae308398e3bd9e6d91eda36186
Hurley <Sarah.Hurley@ct.gov>
2024-05-29 12:22:36 -0400

    add zipcode cleaning code

--- address_data_cleaning.R
+++ address_data_cleaning.R
@@ -6,6 +6,7 @@
 # Load packages used in the script
 library(readr) # readr is package for reading in dataframes from delimited files likes csvs
 library(dplyr) # dplyr is a package for dataframe manipulation 
+library(stringr) # stringr is a package to manipulate strings
 #library (tidyverse) # instead of reading in each of these packages separately, you can also load the tidyverse
 # tidyverse is a collection of packages for data science 
 
@@ -17,6 +18,7 @@ addresses <- read_csv("https://raw.githubusercontent.com/CTOpenData/r-user-group
 ct_name_cleaner <- read_csv("https://raw.githubusercontent.com/CT-Data-Collaborative/ctnamecleaner/master/ctnamecleaner.csv")
 
 # Join the addresses dataframe with the ct_name_cleaner dataframe and bring in realname column
+# & clean the zipcodes
 addresses_cleaned <- addresses %>% 
   # Make town names uppercase using the mutate function
   mutate(Town = toupper(Town)) %>% 
@@ -27,7 +29,17 @@ addresses_cleaned <- addresses %>%
     Town_Messy = Town,
     Town_Standardized = realname
   ) %>% 
+  # Keep just the first 5 characters of the "Zip" column
+  mutate(Zip_clean = str_sub(Zip, 1, 5)) %>% 
   # Reorder the columns
-  select(`Street address`, Town_Messy, Town_Standardized, Zip, County, State)
+  select(
+    `Street address`, 
+    Town_Messy, 
+    Town_Standardized, 
+    Zip, 
+    Zip_clean,
+    County, 
+    State
+    )
 
 
