5d9a4360200209d80fa5895a98d0d7e94e10b1d9
Pauline Zaldonis <45877293+pzaldonis@users.noreply.github.com>
2024-05-29 11:20:48 -0400

    Add files via upload

---
+++ address_data_cleaning.R
@@ -0,0 +1,33 @@
+# R script to clean messy address data 
+
+# If you haven't installed the packages yet, that's the first step 
+# install.packages(c('readr', 'dplyr', 'tidyverse'))
+
+# Load packages used in the script
+library(readr) # readr is package for reading in dataframes from delimited files likes csvs
+library(dplyr) # dplyr is a package for dataframe manipulation 
+#library (tidyverse) # instead of reading in each of these packages separately, you can also load the tidyverse
+# tidyverse is a collection of packages for data science 
+
+# Load the address data, which is hosted on Github, and save it as a dataframe called addresses
+addresses <- read_csv("https://raw.githubusercontent.com/CTOpenData/r-user-group/main/demo_address_data.csv")
+
+# Standardize the town names 
+# Bring in the ctnamecleaner file from Github 
+ct_name_cleaner <- read_csv("https://raw.githubusercontent.com/CT-Data-Collaborative/ctnamecleaner/master/ctnamecleaner.csv")
+
+# Join the addresses dataframe with the ct_name_cleaner dataframe and bring in realname column
+addresses_cleaned <- addresses %>% 
+  # Make town names uppercase using the mutate function
+  mutate(Town = toupper(Town)) %>% 
+  # Join with ctnamecleaner on town name
+  left_join(ct_name_cleaner, by = c("Town" = "name")) %>% 
+  # Rename columns
+  rename(
+    Town_Messy = Town,
+    Town_Standardized = realname
+  ) %>% 
+  # Reorder the columns
+  select(`Street address`, Town_Messy, Town_Standardized, Zip, County, State)
+
+
