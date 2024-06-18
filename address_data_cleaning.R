70c1ee36178a349ea989aa5eef8b4fd6f654a3cb
Chuck <ibecav@gmail.com>
2024-06-18 16:52:44 -0400

    backing out changes

--- address_data_cleaning.R
+++ address_data_cleaning.R
@@ -2,13 +2,12 @@
 
 # If you haven't installed the packages yet, that's the first step 
 # install.packages(c('readr', 'dplyr', 'tidyverse'))
-# pak::pkg_install("tidyverse")
 
 # Load packages used in the script
 library(readr) # readr is package for reading in dataframes from delimited files likes csvs
 library(dplyr) # dplyr is a package for dataframe manipulation 
 library(stringr) # stringr is a package to manipulate strings
-# library (tidyverse) # instead of reading in each of these packages separately, you can also load the tidyverse
+#library (tidyverse) # instead of reading in each of these packages separately, you can also load the tidyverse
 # tidyverse is a collection of packages for data science 
 
 # Load the address data, which is hosted on Github, and save it as a dataframe called addresses
@@ -41,50 +40,6 @@ addresses_cleaned <- addresses %>%
     Zip_clean,
     County, 
     State
-    )
-
-## Okay now we have "corrected addresses" lets
-## put them on a map and show the county and correct
-## town name.
-
-# pak::pkg_install("tidygeocoder")
-# pak::pkg_install("sf")
-# pak::pkg_install("ggrepel")
-
-ct_towns <- sf::st_read("shapefiles/cb_2017_09_cousub_500k.shp")
-ct_counties <- sf::st_read("shapefiles/countyct_37800_0000_1990_s100_CENSUS_1_shp_wgs84.shp")
-
-addresses_cleaned %>% 
-  mutate(plot_address = paste0(`Street address`, ", ", Town_Standardized, ", ", State),
-         Town_Standardized = str_to_title(Town_Standardized)) %>% 
-  tidygeocoder::geocode(address = plot_address, 
-                        # method = "osm") %>% 
-                        method = "bing") %>% 
-                        # method = "census") %>% 
-  ggplot() +
-  geom_sf(data = ct_towns, fill = "white") +
-  geom_sf(data = ct_counties, fill = NA, colour = "darkblue", linewidth = 1)  +
-  # coord_sf(default_crs = sf::st_crs(4326), label_axes = "----") +
-  # borders("county", regions = "connecticut") +
-  geom_point(aes(x = long, y = lat, color = County)) +
-  ggrepel::geom_label_repel(aes(x = long, y = lat, label = Town_Standardized),
-                            size = 2.5,
-                            min.segment.length = .25) +
-  labs(
-    x = NULL, y = NULL
-  ) +
-  theme_minimal() +
-  theme(
-    axis.line = element_blank(),
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank()
   )
 
-ggplot(ct_counties) +
-  geom_sf()
-geom_sf(aes(fill = ALAND))
 
